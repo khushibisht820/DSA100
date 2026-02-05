01859829dcde5a21a32d7e4be613553005d781fb
Khushi Bisht <khushib123456@gmail.com>
2026-02-05 20:13:10 +0530

    update

---
+++ day10.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void merge(int* nums1, int m, int* nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+int main() {
+    int nums1[10] = {1, 3, 5, 7};   
+    int nums2[4]  = {2, 4, 6, 8};   
+
+    int m = 4;
+    int n = 4;
+
+    merge(nums1, m, nums2, n);
+
+    printf("Merged array: ");
+    for (int i = 0; i < m + n; i++) {
+        printf("%d ", nums1[i]);
+    }
+
+    return 0;
+}
